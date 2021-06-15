121d2ec781be724c0a8f87a5b2ad3a274cde0b23
Taranchuk <nfhfyxer@gmail.com>
2021-06-15 15:39:12 +0300

    update

--- 1.2/Source/Rimedieval/RimedievalMod.cs
+++ 1.2/Source/Rimedieval/RimedievalMod.cs
@@ -51,8 +51,31 @@ namespace Rimedieval
                 RimedievalMod.settings.rimedievalMechAddonWasLoaded = true;
                 RimedievalMod.settings.disableMechanoids = false;
             }
+            RemoveTechHediffs();
         }
 
+        public static void RemoveTechHediffs()
+        {
+            var defs = DefDatabase<ThingDef>.AllDefs.Where(x => x.isTechHediff && 
+            ((x.costList?.Any(y => y.thingDef == ThingDefOf.Plasteel || y.thingDef == ThingDefOf.ComponentSpacer) ?? false)
+            || x.techLevel > TechLevel.Industrial
+            || (x.recipeMaker?.recipeUsers?.Any(z => z == ThingDef.Named("TableMachining")) ?? false)
+            )).ToList();
+            MethodInfo methodInfo = AccessTools.Method(typeof(DefDatabase<ThingDef>), "Remove", null, null);
+            foreach (var def in defs)
+            {
+                Log.Message("Removing " + def);
+                methodInfo.Invoke(null, new object[]
+                {
+                    def
+                });
+            }
+            foreach (var def in DefDatabase<ThingDef>.AllDefs.Where(x => x.isTechHediff))
+            {
+                Log.Message("Remained tech hediff: " + def);
+            }
+
+        }
         public static void DoDefsAlter()
         {
 
